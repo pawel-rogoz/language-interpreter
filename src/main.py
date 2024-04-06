3323aeb4943c690fbf167246e615cde471c8258f
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-04-06 17:23:14 +0200

    src package changes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from scanner.scanner import Scanner
+from src.scanner.scanner import Scanner
+from io import StringIO
 
 if __name__ == "__main__":
-    scanner = Scanner()
+    scanner = Scanner(StringIO("int a = 1;"))
     print("Hello world")
