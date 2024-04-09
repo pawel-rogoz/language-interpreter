0150e195e8b601e7abee2a3e3825f1257276aa84
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-04-09 19:23:01 +0200

    Token str representation added

--- src/tokens/token.py
+++ src/tokens/token.py
@@ -3,3 +3,6 @@ class Token:
         self.type = type
         self.position = position
         self.value = value
+
+    def __str__(self):
+        return f"Token Description:\n\tType: {self.type}\n\tPosition:\n\t\tLine: {self.position.line}\n\t\tColumn: {self.position.column}\n\tValue: {self.value}"
