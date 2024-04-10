01ea81b4a153eeb7843c882a8123b1713bec31e5
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-04-10 12:54:58 +0200

    New types of LexerError added

--- src/lexer/lexer_error.py
+++ src/lexer/lexer_error.py
@@ -1,9 +1,40 @@
 from src.scanner.position import Position
 
+
 class LexerError(Exception):
     def __init__(self, message: str, position: Position) -> None:
         self.message = message
         self.position = position
 
     def __str__(self) -> None:
-        return f"ERROR: {self.message}, at line: {self.position.line}, column: {self.position.column}"
+        return f"{self.__class__.__name__}: {self.message}, at line: {self.position.line}, column: {self.position.column}"
+
+
+class IntError(LexerError):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message, position)
+
+
+class FloatError(LexerError):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message, position)
+
+
+class StringError(LexerError):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message, position)
+
+
+class EscapeCharacterError(LexerError):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message, position)
+
+
+class IdentifierError(LexerError):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message, position)
+
+
+class CreateTokenError(LexerError):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message, position)
