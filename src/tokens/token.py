8e31a25756f64ebab3af39259d3a864e62f436fb
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-08 21:02:25 +0200

    visual changes in token class

--- src/tokens/token.py
+++ src/tokens/token.py
@@ -1,5 +1,5 @@
 class Token:
-    def __init__(self, type, position, value = None) -> None:
+    def __init__(self, type, position, value=None) -> None:
         self.type = type
         self.position = position
         self.value = value
