7ddb0407b9ab06c329d5b10e6f02df7f280c9987
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-09 15:17:42 +0200

    block class changes

--- src/parser/classes/block.py
+++ src/parser/classes/block.py
@@ -1,7 +1,15 @@
+from src.parser.classes.statement import *
+
+
 class Block:
-    def __init__(self, statements):
-        self._statements = statements
+    def __init__(self, statements=None):
+        if statements is None:
+            statements = list()
+        self._statements: list[Statement] = statements
+
+    def __eq__(self, other):
+        return self.statements == other.statements
 
     @property
-    def statements(self):
+    def statements(self) -> list[Statement]:
         return self._statements
