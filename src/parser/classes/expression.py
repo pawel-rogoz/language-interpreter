8eb66886861e1ce9c2a9bf32ca7b62cfa1b5715c
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-09 15:16:15 +0200

    type added to literal expression

--- src/parser/classes/expression.py
+++ src/parser/classes/expression.py
@@ -52,8 +52,13 @@ class IndexingExpression(UnaryExpression):
 
 
 class LiteralExpression(Expression):
-    def __init__(self, value):
+    def __init__(self, type, value):
+        self._type = type
         self._value = value
+
+    @property
+    def type(self):
+        return self._type
 
     @property
     def value(self):
