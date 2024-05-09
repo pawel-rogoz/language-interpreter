71b9c38e93100d7634c22a14eab90d3250dd36ac
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-09 19:06:22 +0200

    __eq__ methods added to type objects

--- src/parser/classes/type.py
+++ src/parser/classes/type.py
@@ -6,6 +6,9 @@ class BaseType:
     def __init__(self, type):
         self._type: Type = type
 
+    def __eq__(self, other):
+        return self.type == other.type
+
     @property
     def type(self):
         return self._type
@@ -21,6 +24,9 @@ class KeyValueType(BaseType):
         self._key_type: Type = key_type
         self._value_type: Type = value_type
 
+    def __eq__(self, other):
+        return self.type == other.type and self.key_type == other.key_type and self.value_type == other.value_type
+
     @property
     def key_type(self):
         return self._key_type
@@ -33,11 +39,14 @@ class KeyValueType(BaseType):
 class ElementType(BaseType):
     def __init__(self, type, element_type):
         super().__init__(type)
-        self._value_type: Type = element_type
+        self._element_type: Type = element_type
+
+    def __eq__(self, other):
+        return self.type == other.type and self.element_type == other.element_type
 
     @property
-    def value_type(self):
-        return self._value_type
+    def element_type(self):
+        return self._element_type
 
 
 class Type(Enum):
