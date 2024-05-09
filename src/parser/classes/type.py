3ab9c2be6f89d6425011a4d2f810b6b46369d85e
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-09 15:16:56 +0200

    types structure reorganized

--- src/parser/classes/type.py
+++ src/parser/classes/type.py
@@ -1,24 +1,25 @@
 from abc import ABC, abstractmethod
+from enum import Enum, auto
 
 
-class Type:
+class BaseType:
     def __init__(self, type):
-        self._type = type
+        self._type: Type = type
 
     @property
     def type(self):
         return self._type
 
 
-class FunctionType(Type):
+class FunctionType(BaseType):
     pass
 
 
-class KeyValueType(Type):
+class KeyValueType(BaseType):
     def __init__(self, type, key_type, value_type):
         super().__init__(type)
-        self._key_type = key_type
-        self._value_type = value_type
+        self._key_type: Type = key_type
+        self._value_type: Type = value_type
 
     @property
     def key_type(self):
@@ -29,43 +30,22 @@ class KeyValueType(Type):
         return self._value_type
 
 
-class ValueType(Type):
-    def __init__(self, type, value_type):
+class ElementType(BaseType):
+    def __init__(self, type, element_type):
         super().__init__(type)
-        self._value_type = value_type
+        self._value_type: Type = element_type
 
     @property
     def value_type(self):
         return self._value_type
 
 
-# class PairType(KeyValueType):
-#     pass
-#
-#
-# class DictType(KeyValueType):
-#     pass
-#
-#
-# class ListType(ValueType):
-#     pass
-#
-#
-# class IntType(Type):
-#     pass
-#
-#
-# class FloatType(Type):
-#     pass
-#
-#
-# class StringType(Type):
-#     pass
-#
-#
-# class BoolType(Type):
-#     pass
-#
-#
-# class VoidType(Type):
-#     pass
+class Type(Enum):
+    VOID = auto()
+    INT = auto()
+    FLOAT = auto()
+    BOOL = auto()
+    STRING = auto()
+    PAIR = auto()
+    LIST = auto()
+    DICT = auto()
