d9869ccea84665e76f35520801149993746b2ce1
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-16 18:26:25 +0200

    ParserError changes
    Now constructor takes two more arguments: expected and actual token. Returned error message is based on its values, or message if both token types are not provided

--- src/parser/parser_error.py
+++ src/parser/parser_error.py
@@ -1,13 +1,24 @@
 from src.scanner.position import Position
+from src.tokens.token_type import TokenType
 
 
 class ParserError(Exception):
-    def __init__(self, message: str, position: Position) -> None:
-        self.message = message
-        self.position = position
+    def __init__(self, message, expected_token, actual_token, position) -> None:
+        self.message: str | None = message
+        self.expected_token: TokenType | None = expected_token
+        self.actual_token: TokenType | None = actual_token
+        self.position: Position = position
 
     def __str__(self) -> str:
-        return f"{self.__class__.__name__}: {self.message}, at line: {self.position.line}, column: {self.position.column}"
+        message = ''
+        if self.expected_token and self.actual_token:
+            message += f"Expected {self.expected_token}, got: {self.actual_token}"
+        elif self.message:
+            message += self.message
+        else:
+            message += "Undefined error"
+        message += f", at line {self.position.line}, column: {self.position.column}"
+        return message
 
 
 class FunctionExistsError(ParserError):
