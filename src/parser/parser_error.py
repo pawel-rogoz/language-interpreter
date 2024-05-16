5bdde8c20a1ee9324eb6f9af4894a26652d6b3b6
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-05-16 20:55:46 +0200

    __str__ method of ParserError class changed

--- src/parser/parser_error.py
+++ src/parser/parser_error.py
@@ -3,21 +3,25 @@ from src.tokens.token_type import TokenType
 
 
 class ParserError(Exception):
-    def __init__(self, message, expected_token, actual_token, position) -> None:
+    def __init__(self, position=None, message=None, expected_token=None, actual_token=None) -> None:
+        self.position: Position | None = position
         self.message: str | None = message
-        self.expected_token: TokenType | None = expected_token
+        self.expected_token: set = expected_token
         self.actual_token: TokenType | None = actual_token
-        self.position: Position = position
 
     def __str__(self) -> str:
         message = ''
         if self.expected_token and self.actual_token:
-            message += f"Expected {self.expected_token}, got: {self.actual_token}"
+            expected_tokens = "" if len(self.expected_token) <= 1 else "one of: "
+            for token in self.expected_token:
+                expected_tokens += f"{token}, "
+            message += f"Expected {expected_tokens}got: {self.actual_token}"
         elif self.message:
             message += self.message
         else:
             message += "Undefined error"
-        message += f", at line {self.position.line}, column: {self.position.column}"
+        if self.position:
+            message += f", at line {self.position.line}, column: {self.position.column}"
         return message
 
 
