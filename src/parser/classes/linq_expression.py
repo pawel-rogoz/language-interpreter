5f202dd3a6894f8ec9751c31b3d553904b3e761d
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-06-01 18:29:56 +0200

    LINQExpression refactoring

--- src/parser/classes/linq_expression.py
+++ src/parser/classes/linq_expression.py
@@ -1,8 +1,16 @@
+from typing import TYPE_CHECKING
+
 from src.parser.classes.expression import Expression
 
+from src.interpreter.component import Component
 
-class LINQExpression(Expression):
-    def __init__(self,from_expression,where_expression,orderby_expression,orderby_sorting,select_expression):
+if TYPE_CHECKING:
+    from src.interpreter.visitor import Visitor
+
+
+class LINQExpression(Expression, Component):
+    def __init__(self, from_expression: Expression, where_expression: Expression, orderby_expression: Expression,
+                 orderby_sorting: Expression, select_expression: Expression) -> None:
         self._from_expression = from_expression
         self._where_expression = where_expression
         self._orderby_expression = orderby_expression
@@ -10,21 +18,24 @@ class LINQExpression(Expression):
         self._select_expression = select_expression
 
     @property
-    def from_expression(self):
+    def from_expression(self) -> Expression:
         return self._from_expression
 
     @property
-    def where_expression(self):
+    def where_expression(self) -> Expression:
         return self._where_expression
 
     @property
-    def orderby_expression(self):
+    def orderby_expression(self) -> Expression:
         return self._orderby_expression
 
     @property
-    def orderby_sorting(self):
+    def orderby_sorting(self) -> Expression:
         return self._orderby_sorting
 
     @property
-    def select_expression(self):
+    def select_expression(self) -> Expression:
         return self._select_expression
+
+    def accept(self, visitor: 'Visitor') -> None:
+        visitor.visit_linq_expression(self)
