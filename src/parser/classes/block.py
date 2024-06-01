4e8d6bd0871c29a23d74c381084eeb12fbbd671d
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-06-01 18:23:52 +0200

    Block refactoring

--- src/parser/classes/block.py
+++ src/parser/classes/block.py
@@ -1,21 +1,24 @@
-from src.parser.classes.statement import Statement
+from typing import TYPE_CHECKING
 
-from src.interpreter.visitor import Visitor
 from src.interpreter.component import Component
+
+if TYPE_CHECKING:
+    from src.parser.classes.statement import Statement
+    from src.interpreter.visitor import Visitor
 
 
 class Block(Component):
-    def __init__(self, statements=None):
+    def __init__(self, statements: ['Statement'] = None) -> None:
         if statements is None:
             statements = list()
-        self._statements: list[Statement] = statements
+        self._statements = statements
 
-    def __eq__(self, other):
-        return self.statements == other.statements
+    def __eq__(self, other) -> bool:
+        return self._statements == other.statements
 
     @property
-    def statements(self) -> list[Statement]:
+    def statements(self) -> list['Statement']:
         return self._statements
 
-    def accept(self, visitor: Visitor) -> None:
+    def accept(self, visitor: 'Visitor') -> None:
         visitor.visit_block(self)
