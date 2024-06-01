aee31b0c6ea420f1f574b2486b3061149c75ea14
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-06-01 17:32:04 +0200

    Implementation of Component interface added to FunctionDefinition

--- src/parser/classes/function_definition.py
+++ src/parser/classes/function_definition.py
@@ -1,14 +1,25 @@
-from src.tokens.token_type import TokenType
 from src.parser.classes.parameter import Parameter
 from src.parser.classes.block import Block
+from src.parser.classes.type import BaseType
 from src.scanner.position import Position
-from src.parser.classes.type import Type
+
+from src.interpreter.visitor import Visitor
+from src.interpreter.component import Component
 
 
-class FunctionDefinition:
+class FunctionDefinition(Component):
     def __init__(self, name, type, parameters, block, position):
         self.name: str = name
-        self.type: Type = type
+        self.type: BaseType = type
         self.parameters: list[Parameter] = parameters
         self.block: Block = block
         self.position: Position = position
+
+    def accept(self, visitor: Visitor) -> None:
+        visitor.visit_function_definition(self)
+
+    # def visit(self, scope: Scope):
+    #     result = self.block.visit(scope)
+    #     if result.type != self.type:
+    #         raise InterpreterError()
+    #     return result
