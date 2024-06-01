3844d023b935890627a4ea0d6c47385fc3124df7
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-06-01 18:31:23 +0200

    program.py refactoring

--- src/parser/classes/program.py
+++ src/parser/classes/program.py
@@ -1,8 +1,22 @@
-from src.parser.classes.function_definition import FunctionDefinition
+from typing import TYPE_CHECKING
 
-class Program:
-    def __init__(self, functions: dict):
+from src.interpreter.component import Component
+
+if TYPE_CHECKING:
+    from src.interpreter.visitor import Visitor
+    from src.parser.classes.function_definition import FunctionDefinition
+
+
+class Program(Component):
+    def __init__(self, functions: dict[str, 'FunctionDefinition']):
         self._functions = functions
 
-    def get_functions(self) -> dict[str, FunctionDefinition]:
+    @property
+    def functions(self) -> dict[str, 'FunctionDefinition']:
         return self._functions
+
+    def get_functions(self) -> dict[str, 'FunctionDefinition']:
+        return self._functions
+
+    def accept(self, visitor: 'Visitor') -> None:
+        visitor.visit_program(self)
