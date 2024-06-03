21469b166ce72d82cbcf9f1a5f88a56afe783710
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-06-03 23:31:43 +0200

    value changes

--- src/interpreter/value.py
+++ src/interpreter/value.py
@@ -1,88 +1,77 @@
-from src.parser.classes.type import BaseType, ElementType, KeyValueType
+from src.parser.classes.type import BaseType, ElementType, KeyValueType, Type
 from src.interpreter.interpreter_error import InterpreterError
 from typing import Union
 from abc import ABC, abstractmethod
 
 
 class Value(ABC):
-    def __init__(self, type: BaseType):
+    def __init__(self, type: Union[BaseType, KeyValueType, ElementType], value):
         self._type = type
-
-    @property
-    def type(self):
-        return self._type
-
-    @abstractmethod
-    def change_value(self, value: 'Value'):
-        pass
-
-
-class BaseValue(Value):
-    def __init__(self, type: BaseType, value: Union[str, int, float, bool, list, dict]):
-        super().__init__(type)
         self._value = value
+
+    def __eq__(self, other):
+        return self._value == other.value and self._type == other.type
 
     @property
     def value(self):
         return self._value
 
-    @value.setter
-    def value(self, value: Value):
-        if value.type != self._type:
-            raise InterpreterError()
-        self._value = value
+    @property
+    def type(self):
+        return self._type
 
-    def change_value(self, value: Value):
-        self.value = value
+    def change_value(self, value: 'Value'):
+        if value.type == self.type:
+            self._value = value.value
+        else:
+            raise InterpreterError(message=f"Can't assign value: {value.value} to object type {self._type}")
+
+
+class BaseValue(Value):
+    pass
 
 
 class ElementValue(Value):
-    def __init__(self, type: ElementType, elements_values: [Union[str, int, float, bool, list, dict]]):
-        super().__init__(type)
-        self._elements_values = elements_values
+    def __init__(self, type, value: list):
+        super().__init__(type, value)
 
-    @property
-    def elements_values(self):
-        return self._elements_values
-
-    @elements_values.setter
-    def elements_values(self, value: 'ElementValue'):
-        if self._type != value.type or len(self._elements_values) != len(value.elements_values):
-            raise InterpreterError()
-        self._elements_values = value.elements_values
-
-    def swap_value(self, index: int, value: BaseValue):
+    def change_value(self, index: int, value: Value):
         if self._type != value.type:
             raise InterpreterError()
-        if len(self._elements_values) < index:
+        if len(self._value) < index:
             raise InterpreterError()
-        self._elements_values[index] = value.value
+        self._value[index] = value.value
 
-    def add_value(self, value: BaseValue):
+    def add_value(self, value: Value):
         if self._type != value.type:
             raise InterpreterError()
-        self._elements_values.append(value.value)
+        self._value.append(value.value)
 
-    def change_value(self, value: Value):
-        self._elements_values = value
+    def get_value(self, index: int) -> Value:
+        if index > len(self._value):
+            raise InterpreterError(f"Cannot get a value from index {index} - object size is: {len(self._value)}")
+        return self._value[index]
 
 
 class KeyValueValue(Value):
-    def __init__(self, type: KeyValueType, values: dict):
-        super().__init__(type)
-        self._values = values
+    def __init__(self, type: KeyValueType, value: dict):
+        super().__init__(type, value)
 
-    @property
-    def values(self):
-        return self._values
-
-    @values.setter
-    def values(self, values: 'KeyValueValue'):
-        if self._type != values.type:
+    def change_value(self, key: Value, value: Value):
+        if self._type != value.type:
             raise InterpreterError()
-        self._values = values
+        if key.value in self._value.keys():
+            self._value[key.value] = value.value
+        raise InterpreterError()
 
-    def change_value(self, value: Value):
-        self._values = value
+    def add_value(self, key: Value, value: Value):
+        if self._type.key_type != key.type or self._type.value_type != value.type:
+            raise InterpreterError()
+        if key.value in self._value.keys():
+            raise InterpreterError()
+        self._value[key.value] = value.value
 
-
+    def get_value(self, key) -> Value:
+        if key not in self._value.keys():
+            raise InterpreterError(f"There is no element with key: {key}")
+        return self._value[key]
