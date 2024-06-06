c35f065ea5f981b403377a96a69522c4dee314a0
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-06-06 14:29:29 +0200

    Checking if value is not None and typing added to variable

--- src/interpreter/variable.py
+++ src/interpreter/variable.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from src.interpreter.interpreter_error import InterpreterError
 from src.interpreter.value import BaseValue, Value
 from src.parser.classes.type import BaseType
@@ -7,24 +9,24 @@ class Variable:
     def __init__(self, type: BaseType, id: str, value: Value | None):
         self._type = type
         self._id = id
-        if value.type != self._type:
+        if value is not None and value.type != self._type:
             raise InterpreterError(message=f"Can't assign value type: {value.type} to variable type: {self._type}")
         self._value = value
 
     @property
-    def type(self):
+    def type(self) -> BaseType:
         return self._type
 
     @property
-    def id(self):
+    def id(self) -> str:
         return self._id
 
     @property
-    def value(self):
+    def value(self) -> Optional[Value]:
         return self._value
 
     @value.setter
-    def value(self, value: Value):
+    def value(self, value: Value) -> None:
         if self._type != value.type:
             raise InterpreterError()
         if self._value is None:
